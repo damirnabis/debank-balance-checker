6b0d6e501acad71ce5f1ff688130a12caffaed60
damirnabis <>
2025-11-23 21:28:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-# Количество одновременных потоков (при наличии прокси)
+# Number of simultaneous streams (when a proxy is available) | Количество одновременных потоков (при наличии прокси)
 MAX_CONCURRENT = 5
 
-# Количество попыток на один прокси, после чего пытается через следующий прокси
+# Number of attempts per proxy, after which it tries the next proxy | Количество попыток на один прокси, после чего пытается через следующий прокси
 ATTEMPTS_PER_PROXY = 3
 
-# Пауза между обновленияем данных из Debank (минуты)
+# Pause between data updates from Debank (minutes) | Пауза между обновленияем данных из Debank (минуты)
 UPDATE_DATA_MIN = 15
 
-# Пауза между обновленияем данных на странице браузера (секунды)
+# Pause between data updates on the browser page (seconds) | Пауза между обновленияем данных на странице браузера (секунды)
 UPDATE_HTML_SEC = 5
